5dec5964c57d8f91ed8929ede44628b92b86d639
tweeeety <tweeeety.gmail@gmail.com>
2018-05-31 17:23:15 +0900

    add task_sync

---
+++ task_sync/gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
+gulp.task('task', function(callback) {
+  runSequence('task-1', 'task-2', 'task-3', callback);
+});
+
+gulp.task('task-1', function() {
+  console.log('called task-1');
+});
+
+gulp.task('task-2', function(callback){
+  runSequence('task-2-1', 'task-2-2', callback);
+});
+
+gulp.task('task-2-1', function(callback) {
+  console.log('called task-2-1');
+
+  // 5秒間かかるタスク
+  setTimeout(function(){
+    console.log("\tcalled task-2-1_subtask");
+    callback();
+  }, 5000);
+});
+
+gulp.task('task-2-2', function(callback) {
+  console.log('called task-2-2');
+
+  // 3秒間かかるタスク
+  setTimeout(function(){
+    console.log("\tcalled task-2-2_subtask");
+    callback();
+  }, 3000);
+});
+
+gulp.task('task-3', function(callback) {
+  console.log('called task-3');
+
+  // 1秒間かかるタスク
+  setTimeout(function(){
+    console.log("\tcalled task-3_subtask");
+    callback();
+  }, 1000);
+
+});
+
+gulp.task('default', ['task-sync']);
